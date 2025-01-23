a112a3344cbdbedf93ee41f47b30b002636715a1
Максим Царёв <max@MacBook-Air-Maksim-2.local>
2025-01-23 11:26:47 +0300

    добавил lint

--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@ MIGRATE := migrate -path ./migrations -database $(DB_DSN)
 gen:
 	oapi-codegen -config openapi/.openapi -include-tags tasks -package tasks openapi/openapi.yaml > ./internal/web/tasks/api.gen.go
 
+lint:
+	golangci-lint run --out-format=colored-line-number
+
 migrate-new:
 	migrate create -ext sql -dir ./migrations ${NAME}
 
--- internal/taskServise/orm.go
+++ internal/taskServise/orm.go
@@ -7,37 +7,3 @@ type Task struct {
 	Task   string `json:"task"`    // Название задачи
 	IsDone bool   `json:"is_done"` // Статус выполнения задачи
 }
-
-//
-//func Migrate(db *gorm.DB) error {
-//	return db.AutoMigrate(&Task{})
-//}
-//
-//func CreateTask(db *gorm.DB, task *Task) error {
-//	return db.Create(task).Error
-//}
-//
-//func GetAllTasks(db *gorm.DB) ([]Task, error) {
-//	var tasks []Task
-//	err := db.Find(&tasks).Error
-//	return tasks, err
-//}
-//
-//func GetTaskByID(db *gorm.DB, id uint) (*Task, error) {
-//	var task Task
-//	err := db.First(&task, id).Error
-//	return &task, err
-//}
-//
-//func UpdateTask(db *gorm.DB, id uint, updateData map[string]interface{}) (*Task, error) {
-//	var task Task
-//	err := db.Model(&task).Where("id = ?", id).Updates(updateData).Error
-//	if err != nil {
-//		return nil, err
-//	}
-//	return &task, nil
-//}
-//
-//func DeleteTask(db *gorm.DB, id uint) error {
-//	return db.Delete(&Task{}, id).Error
-//}
